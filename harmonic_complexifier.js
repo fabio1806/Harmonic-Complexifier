cbe6b3e0ca9d322925e7e18c3b8279102af984d9
fabio1806 <fabio123.milan@gmail.com>
2021-01-03 16:42:38 +0100

    Update all

---
+++ harmonic_complexifier.js
@@ -0,0 +1,203 @@
+const BASE_FREQ = 261.63                              //frequency of C4
+keys            = "zsxdcvgbhnjm,q2w3er5t6y7ui";
+
+const keySel = document.getElementById("keySel");
+
+var audio = new AudioContext()
+var gains = {}
+
+function playNote(freq){
+  var osc1 = audio.createOscillator()
+  osc1.type = "triangle"
+  var osc2 = audio.createOscillator()
+  osc2.type = "triangle"
+  var g1 = audio.createGain()
+  var g2 = audio.createGain()
+  gains[freq] = g1
+  gains[4*freq] = g2
+  osc1.connect(g1)
+  osc2.connect(g2)
+  g1.connect(audio.destination)
+  g2.connect(audio.destination)
+  osc1.frequency.setValueAtTime(freq, audio.currentTime)
+  osc2.frequency.setValueAtTime(4*freq, audio.currentTime)
+  osc1.start()
+  osc2.start()
+  g1.gain.linearRampToValueAtTime(1, audio.currentTime)
+  g1.gain.linearRampToValueAtTime(0.8, audio.currentTime+0.2)
+  g2.gain.linearRampToValueAtTime(0.2, audio.currentTime)
+  g2.gain.linearRampToValueAtTime(0.15, audio.currentTime+0.2)
+}
+
+function stopNote(freq){
+  var g1 = gains[freq]
+  g1.gain.linearRampToValueAtTime(0.8, audio.currentTime)
+  g1.gain.linearRampToValueAtTime(0, audio.currentTime+0.2)
+  var g2 = gains[4*freq]
+  g2.gain.linearRampToValueAtTime(0.15, audio.currentTime)
+  g2.gain.linearRampToValueAtTime(0, audio.currentTime+0.2)
+}
+
+function resume() {
+  audio.resume().then(() => {
+    console.log('Playback resumed successfully')
+  })
+}
+
+var chords = new Vue({
+  el: '#root',
+
+  data: {
+
+    keys:       [
+                  {text:'C',  active: false},
+                  {text:'C#', active: false},
+                  {text:'D',  active: false},
+                  {text:'D#', active: false},
+                  {text:'E',  active: false},
+                  {text:'F',  active: false},
+                  {text:'F#', active: false},
+                  {text:'G',  active: false},
+                  {text:'G#', active: false},
+                  {text:'A',  active: false},
+                  {text:'A#', active: false},
+                  {text:'B',  active: false}
+                ],
+
+    notes:       [
+                  {text:'C',  active: false},
+                  {text:'C#', active: false},
+                  {text:'D',  active: false},
+                  {text:'D#', active: false},
+                  {text:'E',  active: false},
+                  {text:'F',  active: false},
+                  {text:'F#', active: false},
+                  {text:'G',  active: false},
+                  {text:'G#', active: false},
+                  {text:'A',  active: false},
+                  {text:'A#', active: false},
+                  {text:'B',  active: false}
+                ],
+
+    chords:     [
+                  {text:'M7',   active: false},
+                  {text:'m7',   active: false},
+                  {text:'7',    active: false},
+                  {text:'mM7',  active: false},
+                  {text:'m7b5', active: false},
+                  {text:'M7#5', active: false},
+                  {text:'dim7', active: false},
+                  {text:'7#5',  active: false},
+                  {text:'7b5',  active: false},
+                  {text:'6#5',  active: false}
+                ],
+
+    durations:  [
+                  {text:'1',    active: false},
+                  {text:'1/2',  active: false},
+                  {text:'1/4',  active: false},
+                  {text:'1/8',  active: false},
+                  {text:'1/16', active: false},
+                  {text:'1/32', active: false},
+                  {text:'1/64', active: false}
+                ],
+
+    preview:    { message: '' },
+
+    stdin:      []
+
+  },
+
+  methods: {
+    activate: function(el, dataName) {
+
+      dataName.forEach((i) => {
+        i.active = false;
+      });
+
+      el.active = true;
+
+      chords.preview.message = inText();
+    }
+  }
+})
+
+var captions = new Vue({
+  el: '#caption',
+  data: {
+    captions: [
+                {text:  'M = major chord'},
+                {text:  'm = minor chord'}
+              ]
+  }
+})
+
+function inText() {
+  text = '';
+
+  chords.notes.forEach((note) => {
+    if (note.active){
+      text = text + note.text
+    }
+  });
+
+  chords.chords.forEach((chord) => {
+    if (chord.active){
+      text = text + chord.text
+    }
+  });
+
+  chords.durations.forEach((time) => {
+    if (time.active){
+      text = text + ' (' + time.text + ')';
+    }
+  });
+
+  return text;
+}
+
+function addToStdin() {
+  previewText = inText()
+  if (previewText != ''){
+    chords.stdin.push(previewText);
+    console.log("Added");
+  }
+  else {
+    console.log("Empty input")
+  }
+}
+
+
+function switchToKeyboard() {
+  buttons.style.display   = 'none';
+  keyboard.style.display  = 'block';
+  stdin.style.display     = 'block';
+  preview.style.display   = 'none';
+  console.log("Switched to keyboard input");
+  document.getElementById("keySwitch").classList.add("activated");
+  document.getElementById("btnSwitch").classList.remove("activated");
+}
+
+function switchToButtons() {
+  buttons.style.display   = 'block';
+  keyboard.style.display  = 'none';
+  stdin.style.display     = 'block';
+  preview.style.display   = 'block';
+  console.log("Switched to buttons input");
+  document.getElementById("keySwitch").classList.remove("activated");
+  document.getElementById("btnSwitch").classList.add("activated");
+}
+
+document.onkeydown = function(e) {
+  if (keyboard.style.display == 'block' && !e.repeat && keys.indexOf(e.key) != -1)
+    playNote( BASE_FREQ * Math.pow(2, keys.indexOf(e.key)/12))
+
+}
+
+document.onkeyup = function(e) {
+  if (keyboard.style.display == 'block' && keys.indexOf(e.key) != -1)
+    stopNote( BASE_FREQ * Math.pow(2, keys.indexOf(e.key)/12))
+}
+
+switchToButtons();
+resume()
